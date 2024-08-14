5f3557c2151ec47fcdff1f86c7ad377bec2ae307
sanikapatil917 <sanikapatil2934@gmail.com>
2024-08-14 06:49:03 +0000

    dt-summ.js

---
+++ basics/dt-summ.js
@@ -0,0 +1,30 @@
+//primitive
+// 7 types- string,boolean,number,bigInt,null,undefined,symbol
+const score=100
+const scoreValue=100.9
+const IsLoggedIn=false
+const temp=null
+let email;
+const id=Symbol(12)
+const anatherId=Symbol(12)
+
+// console.log(id==anatherId)
+
+const bigNumb=123655455
+
+//non-pri(reference)
+//array,objects,functions
+const heros=["shatiman","nagraj"];
+let myObj={
+    name="sanika",
+    age=21,
+}
+
+const myFunction=function()
+    {
+        // console.log("hello")
+    }
+
+// +++++++++++++++++++++++++MEMORY+++++++++++++++++++++++++++++++    
+//stack(primitive),heap(non-pre)
+
