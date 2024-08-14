388be92545686115540757cfb7ea996b2d2c0ffb
sanikapatil917 <sanikapatil2934@gmail.com>
2024-08-14 07:35:09 +0000

    num-maths.js

---
+++ basics/num-maths.js
@@ -0,0 +1,27 @@
+const score=400
+// console.log(score)
+
+const balance=new Number(100)
+// console.log(balance.toFixed(1))
+const otherNumb=20.321
+// console.log(otherNumb.toPrecision(3))
+const hundred=1000000
+// console.log(hundred.toLocaleString('en-In'))
+
+//==============MATHS===================
+console.log(Math)
+console.log(Math.abs(4))
+console.log(Math.round(4.7))
+console.log(Math.floor(4.6))
+console.log(Math.ceil(4.2))
+console.log(Math.min(4,8,7,2))
+console.log(Math.max(4,8,7,2))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+
+const min=10
+const max=40
+
+console.log(Math.random()*(max-min+1))
+console.log(Math.floor(Math.random()*(max-min+1))+min)
