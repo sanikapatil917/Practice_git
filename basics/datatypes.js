c6ca4aff33dad324f39c346509220ef6884f5544
sanikapatil917 <sanikapatil2934@gmail.com>
2024-08-14 04:54:55 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+console.log(3+3);
+
+console.log("Sanika");
+
+let name="Sanika Patil"
+let age=21
+let IsLoggedIn=false
+let state;
+
+// numb =2 power of 52
+//bigInt
+//string""
+//boolean=true/false
+//null=standalone value
+//undefined=
+//symbol=unique
+
+//object
+
+console.log(typeof"sanika")
+console.log(typeof age)
+console.log(typeof null)
