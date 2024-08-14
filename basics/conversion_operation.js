31560c0f391d118d1504389ccacc946a5d8f5cb5
sanikapatil917 <sanikapatil2934@gmail.com>
2024-08-14 05:47:00 +0000

    conversion_operation.js

---
+++ basics/conversion_operation.js
@@ -0,0 +1,53 @@
+let score="Sanika"
+//console.log(typeof score)
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33"=33
+// "33abc" = Nan
+// true= 1;  false=0;
+
+let IsLoggedIn=" "
+
+let booleanIsLoggedIn=Boolean (IsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1=true; 0=false;
+// ""=false;
+// "sanika"=true;
+
+let numb=33
+let stringnumb=String(numb)
+//console.log(numb);
+//console.log(typeof stringnumb);
+
+//**************************Opration***********************//
+let value=3
+let negValue= -value
+//console.log(negValue)
+
+//basic maths operations
+
+//console.log(2+2)
+//console.log(2*2)
+
+let str1="hello!"
+let str2=" Sanika"
+let str3=str1+str2;
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(2+"2")
+// console.log(1+2+"2")
+// console.log((2+2)(3*4))
+
+console.log(+true)
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
