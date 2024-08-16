f47626dd684c5ca6ae4b6b8c427f066204baf966
sanikapatil917 <sanikapatil2934@gmail.com>
2024-08-16 05:05:08 +0000

    date-time.js

---
+++ basics/date-time.js
@@ -0,0 +1,7 @@
+let date=new Date()
+// console.log(date.toString);
+// console.log(date.toDateString);
+// console.log(date.toLocaleDateString);
+
+let myDate=new Date(2023,0,31,5,2)
+console.log(myDate.toLocaleString());
